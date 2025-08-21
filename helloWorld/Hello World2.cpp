5ebc06bd12dc599d8e23e4d51bea5a264d6f3eaf
salma01-yos <salma.staper01@uptc.edu.co>
2025-08-21 16:32:14 +0000

    first commit

---
+++ helloWorld/Hello World2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+    cout << "Hola Mundo!";
+    cout << "Soy una ingeniera electrónica" << endl;
+    cout << "Soy Salma Staper" << endl; 
+
+    return 0; 
+}
