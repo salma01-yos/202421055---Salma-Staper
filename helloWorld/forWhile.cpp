925f77ce234ff8fe6e3c6b0ac7bc1750b008972b
salma01-yos <salma.staper01@uptc.edu.co>
2025-09-02 20:00:54 +0000

    forWhile.cpp

---
+++ helloWorld/forWhile.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    // int i=0
+    // i<10 hasta que se deje 
+   int userNumber;
+
+   cout << "Ingrese un numero: ";
+   cin >> userNumber;
+
+   for (i = 0; i <= 10; i++)
+   {
+        int multiplicationResult = userName * i;
+        cout << userNumber << " * " << i << " = " << endl;
+   }
+   
+
+    
+    return 0;
+}
